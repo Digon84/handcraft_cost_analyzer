7383a7ed6c72d82797e363355081f4b28ff6c2ab
Marcin Rudnicki <m.rudnicki84@gmail.com>
2023-08-29 14:31:04 +0200

    Add detting values from columns

--- src/widgets/add_from_file_inventory_widget.py
+++ src/widgets/add_from_file_inventory_widget.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from PyQt6.QtCore import Qt
 from PyQt6 import uic
-from PyQt6.QtGui import QPalette, QBrush, QColor, QColorConstants
-from PyQt6.QtSql import QSqlRelationalTableModel
+from PyQt6.QtGui import QBrush, QColor, QColorConstants
+from PyQt6.QtSql import QSqlRelationalTableModel, QSqlQuery
 from PyQt6.QtWidgets import QDialog, QFileDialog, QTableWidget, QTableWidgetItem
 
 
@@ -14,26 +16,20 @@ class AddFromFileInventoryWidget(QDialog):
                         range(self.table_model.columnCount())]
         self.add_columns()
         self.add_row()
-        # self.get_values()
+        for column in self.columns:
+            print(self.get_distinct_values_for_column(column))
         self.show()
 
     def add_columns(self):
         table_widget: QTableWidget = self.ui.tableWidget
         table_widget.setColumnCount(len(self.columns))
         table_widget.setHorizontalHeaderLabels(self.columns)
-        print(table_widget.columnCount())
-        print(self.columns)
 
     def add_row(self):
         table_widget: QTableWidget = self.ui.tableWidget
         table_widget.insertRow(table_widget.rowCount())
         table_widget.setItem(table_widget.rowCount()-1, 0, QTableWidgetItem("test"))
         table_widget.item(0, 0).setBackground(QBrush(QColor(QColorConstants.Red)))
-
-    def get_values(self):
-        self.table_model.setTable("items")
-        self.table_model.setQuery("SELECT distinct(material) FROM items")
-
 
     def load_from_file_load_pressed(self):
         file_dialog = QFileDialog()
@@ -59,3 +55,12 @@ class AddFromFileInventoryWidget(QDialog):
             self.ui.lineEdit.setText(data)
 
         print("load_from_file_text_window_clicked")
+
+    @staticmethod
+    def get_distinct_values_for_column(column_name) -> List:
+        result = []
+        sql_select_query = QSqlQuery()
+        sql_select_query.exec(f"SELECT distinct({column_name}) FROM items")
+        while sql_select_query.next():
+            result.append(sql_select_query.value(0))
+        return result
