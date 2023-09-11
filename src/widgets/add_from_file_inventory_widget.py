89ab7e231d24905cdff57d844590ab7bf773cf65
Marcin Rudnicki <m.rudnicki84@gmail.com>
2023-09-11 13:14:24 +0200

    Added possibility to copy data from add from file table to main table

--- src/widgets/add_from_file_inventory_widget.py
+++ src/widgets/add_from_file_inventory_widget.py
@@ -15,12 +15,14 @@ class AddFromFileInventoryWidget(QDialog):
     def __init__(self, table_model: QSqlRelationalTableModel):
         super(QDialog, self).__init__()
         self.table_model = table_model
+        self.first_table_fill = True
         self.ui = uic.loadUi("src/ui/load_from_file_widget.ui", self)
         self.columns_mapping = {self.table_model.headerData(i, Qt.Orientation.Horizontal): i for i in
                                 range(self.table_model.columnCount())}
         print(self.columns_mapping)
         self.shopping_summary_parser = ShoppingSummaryParser()
         self.add_columns()
+
         # for column in self.columns:
         #     print(self.get_distinct_values_for_column(column))
         self.show()
@@ -41,6 +43,7 @@ class AddFromFileInventoryWidget(QDialog):
                 table_widget.item(next_item, column_number).setBackground(QBrush(QColor(QColorConstants.Red)))
 
     def load_from_file_load_pressed(self):
+        self.first_table_fill = True
         file_dialog = QFileDialog()
         parsed_items = []
         if file_dialog.exec():
@@ -50,10 +53,31 @@ class AddFromFileInventoryWidget(QDialog):
                 parsed_items.extend(self.shopping_summary_parser.parse_file(file))
         for parsed_item in parsed_items:
             self.add_row(parsed_item)
-
+        self.first_table_fill = False
 
     def load_from_file_table_edit(self):
         print("load_from_file_table_edit")
+
+    def cell_content_changed(self, row, column):
+        if not self.first_table_fill:
+            table_widget: QTableWidget = self.ui.tableWidget
+            table_widget.item(row, column).setBackground(QBrush(QColor(QColorConstants.Yellow)))
+
+    def accept(self):
+        table_widget: QTableWidget = self.ui.tableWidget
+
+        row_count = table_widget.rowCount()
+        column_count = table_widget.columnCount()
+
+        if row_count > 0:
+            for i in range(row_count):
+                record = self.table_model.record()
+                for j in range(column_count):
+                    record.setValue(table_widget.horizontalHeaderItem(j).text(), table_widget.item(i, j).text())
+                    self.table_model.insertRecord(-1, record)
+            self.table_model.submitAll()
+            self.table_model.select()
+        super().accept()
 
     # TODO: remove?
     def load_from_file_text_window_clicked(self):
