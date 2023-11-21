d6721e5e13934ac806c8f97afa64cfc81a2caab4
Marcin Rudnicki <m.rudnicki84@gmail.com>
2023-11-21 21:02:22 +0100

    [HCA-49] Remove blank lines after export

--- src/file_operations/csv_exporter.py
+++ src/file_operations/csv_exporter.py
@@ -4,6 +4,6 @@ from csv import DictWriter
 def write_content_to_csv(file_path, data_to_write):
     field_names = list(data_to_write[0].keys())
     with open(file_path, "w", encoding='utf-8') as f:
-        writer = DictWriter(f, fieldnames=field_names, delimiter=";")
+        writer = DictWriter(f, fieldnames=field_names, delimiter=";", lineterminator='\n')
         for row in data_to_write:
             writer.writerow(row)
