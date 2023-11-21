8e5a5d6ff6ce1589e3d84b29044ffaff3360a1e0
Marcin Rudnicki <m.rudnicki84@gmail.com>
2023-11-21 21:46:45 +0100

    [HCA-3] Add searching bu multiple strings

--- src/proxy_models/inventory_filter_proxy_model.py
+++ src/proxy_models/inventory_filter_proxy_model.py
@@ -8,9 +8,18 @@ class InventoryFilterProxyModel(QSortFilterProxyModel):
 
     def filterAcceptsRow(self, source_row: int, source_parent: QModelIndex) -> bool:
         if self.filter != "":
-            for column in range(self.sourceModel().columnCount()):
+            filter_elements = self.filter.split(" ")
+            matched_elements = 0
+            elements = []
+            # TODO: skip columns with indexes, make it easier to maintain
+            for column in range(2, self.sourceModel().columnCount()):
                 first_column_index = self.sourceModel().index(source_row, column, source_parent)
-                if self.filter in str(self.sourceModel().data(first_column_index)):
+                element = str(self.sourceModel().data(first_column_index))
+                elements.append(element)
+                for filter_element in filter_elements:
+                    if filter_element in str(self.sourceModel().data(first_column_index)):
+                        matched_elements += 1
+                if matched_elements == len(filter_elements):
                     return True
                 else:
                     continue
