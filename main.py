99d32f8c024d1585f0bae7b20bd48730e8e00a1e
Marcin Rudnicki <m.rudnicki84@gmail.com>
2023-11-22 21:50:32 +0100

    Fix DPI issue

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
+import os
 from src.widgets.main_window import MainWindow
+
+os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
 
 app = QApplication(sys.argv)
 
